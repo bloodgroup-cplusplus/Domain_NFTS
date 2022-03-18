6dd0e5873a43cf53c6eea7f0ad272d9aaa5edac7
Bhushan <tucater10896601@protonmail.com>
2022-03-18 09:23:18 +0530

    modified run.js by adding extra lines for calling the Domains.sol contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,18 @@
 const main = async() =>
 {
+    const [owner,radnomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+    
     const domainContract = await domainContractFactory.deploy();
+
     await domainContract.deployed();
     console.log("Contract deployeed to ", domainContract.address);
-};
+    console.log("Contract deployed by:",owner.address);
+    const txn = await domainContract.register("doom");
+    await txn.wait();
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("owner of domain :",domainOwner);
+}
 
 
 const runMain = async() =>{
