234637963494280a0804cd0dca6a8884041bb9ac
Bhushan <tucater10896601@protonmail.com>
2022-03-18 09:39:16 +0530

    Added funcitons get record and set record

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -6,6 +6,8 @@ contract Domains
 {
     // A "mapping" data type to store their names
     mapping(string => address) public domains;
+    //checkout our new mapping! this will store values 
+    mapping(string =>string) public records;
     constructor()
     {
         console.log("This is my domains contract.Noice.");
@@ -24,6 +26,19 @@ contract Domains
     {
         return domains[name];
     }
+
+    function setRecord(string calldata name, string calldata record) public
+    {
+        //Check that the owner is the transaction sender
+        require (domains[name]==msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord (string calldata name) public view returns (string memory)
+    {
+        return records[name];
+    }
+
 
 }
 
