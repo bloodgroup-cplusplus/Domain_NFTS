6fc7a18ae4ba27539584e83eb1430f8176a56ab0
Bhushan <tucater10896601@protonmail.com>
2022-03-18 08:55:49 +0530

    Added domains.sol file containing the essentail contracts for my domain

---
+++ contracts/Domains.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains
+{
+    constructor()
+    {
+        console.log("This is my domains contract.Noice.");
+
+    }
+}
+
+
