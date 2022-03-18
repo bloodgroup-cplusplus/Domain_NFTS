7d82674c24c12e2ba6da1cfb628389fd426c1cd7
cl-rs <81256027+cl-rs@users.noreply.github.com>
2022-03-18 13:48:01 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.10",
+  networks:{
+    
+    mumbai: {
+        url: "YOUR_ALCHEMY_MUMBAI_URL",
+        accounts: ["YOUR_TEST_WALLET_PRIVATE_KEY"],
+		  }
 };
